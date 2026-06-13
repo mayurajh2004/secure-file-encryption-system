409c4a0e0f676225cb8e902b30d40aff8e12b8a8
MAYU-004 <vmayurajh@gmail.com>
2026-06-13 22:20:47 +0530

    Add src/__init__.py - Make src a Python package

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+__init__.py for src package
+Makes src directory a Python package
+"""
+
+__version__ = "1.0.0"
+__author__ = "Mayuraj H"
+__description__ = "Secure File Encryption System"
